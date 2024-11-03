57620a7a039695785befd698c60ce183b07f5df3
Gabriel Lopes <136989000+GabriielLopes@users.noreply.github.com>
2024-11-03 18:11:41 -0300

    Correções no timer. Correções na sincronização com o relogio.

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -15,10 +15,19 @@ interface Props {
 
 export function PomodoroTimer(props: Props): JSX.Element {
   const [tempoPrincipal, setTempoPrincipal] = useState(props.tempoPomodoro);
-  const [graus, setGraus] = useState((tempoPrincipal / props.tempoPomodoro) * 360);
+  const [graus, setGraus] = useState(360);
   const [contagemTempo, setContagemTempo] = useState(false);
   const [trabalhando, setTrabalhando] = useState(false);
   const circulo = document.querySelector('.circulo') as HTMLDivElement;
+
+  useInterval(
+    () => {
+      setTempoPrincipal((prevTempo) => prevTempo - 1);
+      const qtdePDiminuir = 360 / props.tempoPomodoro;
+      setGraus(graus - qtdePDiminuir);
+    },
+    contagemTempo ? 1000 : null,
+  );
 
   useEffect(() => {
     if (trabalhando) {
@@ -32,23 +41,17 @@ export function PomodoroTimer(props: Props): JSX.Element {
     if (circulo) {
       if (document.querySelectorAll('.working').length > 0) {
         circulo.style.background = `conic-gradient( #EF5D50 ${graus}deg, #e5e7eb 0deg)`;
+        circulo.style.transition = `background 300ms ease-in-out`;
       } else {
         circulo.style.background = `conic-gradient( #41e1ba ${graus}deg, #e5e7eb 0deg)`;
+        circulo.style.transition = `background 300ms ease-in-out`;
       }
     }
-  }, [circulo, graus, trabalhando]);
-
-  useInterval(
-    () => {
-      setTempoPrincipal((prevTempo) => prevTempo - 1);
-      setGraus((tempoPrincipal / props.tempoPomodoro) * 360);
-    },
-    contagemTempo ? 1000 : null,
-  );
+  }, [tempoPrincipal, graus, circulo, trabalhando]);
 
   const configurarTrabalhar = () => {
     setTrabalhando(!trabalhando);
-    setContagemTempo(!contagemTempo);
+    setContagemTempo(true);
   };
 
   return (
@@ -59,11 +62,17 @@ export function PomodoroTimer(props: Props): JSX.Element {
       <div className="controls">
         <Button
           className="btn"
-          texto={!contagemTempo ? 'Trabalhar' : 'Pausar'}
+          texto="Trabalhar"
           onClick={() => configurarTrabalhar()}
         />
         <Button className="btn" texto="teste" onClick={() => console.log('teste')} />
-        <Button className="btn" texto="teste" onClick={() => console.log('teste')} />
+        {trabalhando ? (
+          <Button
+          className="btn"
+          texto={contagemTempo ? 'Pausar' : 'Continuar'}
+          onClick={() => setContagemTempo(!contagemTempo)}
+        />
+        ) : ""}
       </div>
 
       <div className="details">
